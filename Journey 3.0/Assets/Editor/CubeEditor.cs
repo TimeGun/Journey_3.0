2eec555b1f4fe91f4047df67b8648e3c2578f12c
samuelhegner <C16439262@mydit.ie>
2019-09-29 13:46:11 +0100

    Messing with custom editor

---
+++ Journey 3.0/Assets/Editor/CubeEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColourChanger))]
+public class CubeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ColourChanger colourChanger = (ColourChanger)target;
+        
+        GUILayout.Label("Change the Colour of the Cube and Reset it");
+        GUILayout.BeginHorizontal();
+        
+        if (GUILayout.Button("Generate Colour"))
+        {
+            colourChanger.ChangeColour();
+        }
+        
+        if (GUILayout.Button("Reset Colour"))
+        {
+            colourChanger.ResetColour();
+        }
+        
+        GUILayout.EndHorizontal();
+        
+        
+    }
+}
---
+++ Journey 3.0/Assets/Scripts/ColourChanger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ColourChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer renderer;
+
+    public void ChangeColour()
+    {
+        renderer.sharedMaterial.color = Random.ColorHSV();
+    }
+
+    public void ResetColour()
+    {
+        renderer.sharedMaterial.color = Color.white;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeColour();
+        }
+    }
+}
