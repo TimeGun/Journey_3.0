a9b65c64fd6b82cc586eb67b2b2bc3e8d358012e
gazor117 <oreillygary7@gmail.com>
2019-10-30 18:23:42 +0000

    Timeline switching implemented

---
+++ Journey 3.0/Assets/DetectPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    
+    private bool playerDetected;
+
+    public bool PlayerDetected
+    {
+        get { return playerDetected; }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(playerDetected);
+        print(PlayerDetected);
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            playerDetected = true;
+        }
+    }
+}
