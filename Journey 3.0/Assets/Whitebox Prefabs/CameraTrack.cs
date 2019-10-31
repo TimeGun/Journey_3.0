fafaff2a2cf30e4c2d9f16fbb8ec092f06fd738a
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-10-31 14:07:25 +0000

    Cave Whitebox Update
    -Size changing
    -More temp objects added

---
+++ Journey 3.0/Assets/Whitebox Prefabs/CameraTrack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+    public Transform player;
+    float value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value = player.position.z - 67;
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + (value * 0.4f)); 
+    }
+}
