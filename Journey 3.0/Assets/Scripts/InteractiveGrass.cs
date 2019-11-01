795f0f61d9201d2bee76f767e341dabd782dd284
samuelhegner <C16439262@mydit.ie>
2019-11-01 00:10:10 +0000

    Grass Shader Start

---
+++ Journey 3.0/Assets/Scripts/InteractiveGrass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveGrass : MonoBehaviour
+{
+    public Material[] materials;
+    public Transform player;
+    private Vector3 position;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UpdateMaterials());
+    }
+
+    IEnumerator UpdateMaterials()
+    {
+        while (true)
+        {
+            position = player.transform.position;
+            for (int i = 0; i < materials.Length; i++)
+            {
+                materials[i].SetVector("_position", position);
+            }
+
+            yield return null;
+        }
+    }
+}
