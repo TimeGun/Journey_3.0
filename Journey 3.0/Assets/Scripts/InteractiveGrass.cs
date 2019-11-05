c16e4822b5dc92a54a6fc08ede7cfd22c2e68459
samuelhegner <C16439262@mydit.ie>
2019-11-05 00:56:44 +0000

    Grass Shader near complete (Find way of creating oval collider)

--- Journey 3.0/Assets/Scripts/InteractiveGrass.cs
+++ Journey 3.0/Assets/Scripts/InteractiveGrass.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class InteractiveGrass : MonoBehaviour
     public Material[] materials;
     public Transform player;
     private Vector3 position;
+    private float radius;
     
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,21 @@ public class InteractiveGrass : MonoBehaviour
         while (true)
         {
             position = player.transform.position;
+            position.y += player.localScale.y/2f;
             for (int i = 0; i < materials.Length; i++)
             {
                 materials[i].SetVector("_position", position);
+                radius = materials[i].GetFloat("_range");
             }
 
             yield return null;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        Gizmos.color = new Color(0, 0, 1, 0.3f);
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y + (player.localScale.y/2f), transform.position.z), radius);
     }
 }
