87021559437794ed4975e6769f9a0f713c58c380
gazor117 <oreillygary7@gmail.com>
2019-11-06 15:44:11 +0000

    Camera Changes

---
+++ Journey 3.0/Assets/TrackingCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class TrackingCamera : MonoBehaviour
+{
+    public GameObject CameraDolly;
+    private float pathPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // CameraDolly.GetComponent<TimelinePlayable>().
+    }
+}
