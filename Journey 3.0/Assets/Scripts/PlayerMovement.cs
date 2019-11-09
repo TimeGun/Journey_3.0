f261b292eb1ec7d42581b6ab98d0918632d6490a
samuelhegner <C16439262@mydit.ie>
2019-11-09 16:27:18 +0000

    Character Controller Update

--- Journey 3.0/Assets/Scripts/PlayerMovement.cs
+++ Journey 3.0/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 1. Movement based of camera
+/// 2. stop and face dircetion when input is absent
+/// </summary>
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,9 +14,18 @@ public class PlayerMovement : MonoBehaviour
     
     private Rigidbody _rb;
     
-    [SerializeField] private float _speed;
+    [SerializeField] private float _velocity = 5f;
+    [SerializeField] private float _turnSpeed = 10f;
 
-    [SerializeField, Range(0.01f, 0.99f)] private float percentageVelocityToRemove = 0.1f;
+    private Vector2 input;
+
+    private float angle;
+
+    private Quaternion targetRotation;
+    
+ 
+    
+    
     void Start()
     {
         if (cam == null)
@@ -20,25 +34,54 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        Vector3 right = cam.right;
+        GetInput();
         
-        Vector3 forward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+        if(Mathf.Abs(input.x) < 0.1f && Mathf.Abs(input.y) < 0.1f) return;
         
-        Vector3 movement = right * (Input.GetAxis("Horizontal") * _speed) + forward * (Input.GetAxis("Vertical") * _speed) ;
-        
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
 
-        if (movement != Vector3.zero)
-        {
-            _rb.AddForce(movement, ForceMode.VelocityChange);
-            _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _speed);
-            transform.rotation = Quaternion.LookRotation(_rb.velocity, transform.up);
-        }
-        else
-        {
-            _rb.velocity = _rb.velocity * (1f - percentageVelocityToRemove);
-        }
+    /// <summary>
+    ///  Input based of new Input system
+    /// </summary>
+    void GetInput()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
     }
     
+    /// <summary>
+    /// Calculates the direction of movement
+    /// </summary>
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+    
+    /// <summary>
+    /// Rotate towards the target direction
+    /// </summary>
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _turnSpeed * Time.deltaTime);
+    }
+    
+    /// <summary>
+    /// Move along forward axis
+    /// </summary>
+    void Move()
+    {
+        //_rb.velocity += transform.forward * _velocity;
+        //_rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _velocity);
+
+        transform.position += transform.forward * _velocity * Time.deltaTime;
+        
+    }
 }
