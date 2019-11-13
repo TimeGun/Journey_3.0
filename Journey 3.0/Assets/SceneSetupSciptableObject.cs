9bd73a31aa8390aae35498380ab30b1bc55cefe5
samuelhegner <C16439262@mydit.ie>
2019-11-13 23:27:19 +0000

    Working version of github
    https://gist.github.com/svermeulen/8927b29b2bfab4e84c950b6788b0c677

--- Journey 3.0/Assets/SceneSetupSciptableObject.cs
+++ Journey 3.0/Assets/SceneSetupSciptableObject.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class SceneSetupSciptableObject : ScriptableObject
 {
-    
+    public SceneSetup[] sceneSetups;
 }
