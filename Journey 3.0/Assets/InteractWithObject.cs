907e605edd4b632c5652255a82dab20ea236304a
samuelhegner <C16439262@mydit.ie>
2019-11-15 22:20:11 +0000

    Start of Interactible remake

--- Journey 3.0/Assets/InteractWithObject.cs
+++ Journey 3.0/Assets/InteractWithObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpObjects : MonoBehaviour
+public class InteractWithObject : MonoBehaviour
 {
     private ObjectDetection _objectDetection;
 
--- Journey 3.0/Assets/PickUpObject.cs
+++ Journey 3.0/Assets/PickUpObject.cs
@@ -2,26 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteraibleObject : MonoBehaviour, IPickup
+public class PickUpObject :  InteractibleObject
 {
-    [SerializeField] private bool _grabbed;
-
     private Vector3 _stayPosition;
+
+    private Rigidbody _rb;
 
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
 
     public void GetPickedUp(Transform parent)
     {
         _grabbed = true;
-        parent.root.GetComponent<PickUpObjects>().SetGrabObject(gameObject);
+        
+        parent.root.GetComponent<InteractWithObject>().SetGrabObject(gameObject);
     }
 
     public void GetDropped()
     {
         _grabbed = false;
+    }
+
+    public override void StartInteraction()
+    {
     }
 }
