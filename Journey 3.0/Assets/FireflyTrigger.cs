3d2f0a593480e7e3b0d5b26efe28efeb2518f3fb
samuelhegner <C16439262@mydit.ie>
2019-11-17 12:29:00 +0000

    Firefly Prefab

---
+++ Journey 3.0/Assets/FireflyTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class FireflyTrigger : MonoBehaviour
+{
+    
+    [SerializeField] private float _fleeSpeed;
+    
+    [SerializeField] private Transform _flies;
+
+    [SerializeField] private bool flee;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Test");
+        StartCoroutine(Flee());
+    }
+
+    IEnumerator Flee()
+    {
+        flee = true;
+        while (flee)
+        {
+            _flies.transform.Translate(Vector3.up * (_fleeSpeed * Time.deltaTime));
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+}
