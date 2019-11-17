12b586c2fb6dffc11c4482f9103817a9b581d47d
samuelhegner <C16439262@mydit.ie>
2019-11-17 12:40:11 +0000

    Basic Firefly prefab finished

--- Journey 3.0/Assets/FireflyTrigger.cs
+++ Journey 3.0/Assets/FireflyTrigger.cs
@@ -13,23 +13,43 @@ public class FireflyTrigger : MonoBehaviour
     [SerializeField] private float _fleeSpeed;
     
     [SerializeField] private Transform _flies;
+    [SerializeField] private ParticleSystem _system;
+    private ParticleSystem.EmissionModule _em;
+    private ParticleSystem.MainModule _mm;
 
-    [SerializeField] private bool flee;
+    private bool _flee;
+
+    [SerializeField] private float _timeToFleeFor;
+    
+    private float _timeSinceFlee;
     
     private void OnTriggerEnter(Collider other)
     {
-        print("Test");
         StartCoroutine(Flee());
     }
 
     IEnumerator Flee()
     {
-        flee = true;
-        while (flee)
+        _flee = true;
+        while (_flee)
         {
             _flies.transform.Translate(Vector3.up * (_fleeSpeed * Time.deltaTime));
+            _timeSinceFlee += Time.deltaTime;
+
+            if (_timeSinceFlee > _timeToFleeFor)
+            {
+                _flee = false;
+            }
             yield return new WaitForEndOfFrame();
         }
 
+        _em = _system.emission;
+        _mm = _system.main;
+        
+        _em.rateOverDistance = 0;
+        _em.rateOverTime = 0;
+        
+        yield return new WaitForSeconds(_mm.startLifetime.Evaluate(1));
+        Destroy(gameObject);
     }
 }
