ef9108f6ccdcfe3285f26616528a218900b47f99
gazor117 <oreillygary7@gmail.com>
2019-11-23 21:29:50 +0000

    Mountain Cam tracking back and Field Cam set up

--- Journey 3.0/Assets/MixingCamera.cs
+++ Journey 3.0/Assets/MixingCamera.cs
@@ -11,6 +11,7 @@ public class MixingCamera : MonoBehaviour
     public float initialBottomWeight = 20f;
     public AxisEnum axisToTrack;
     private bool playerDetected;
+    public GameObject ReferencePos;
     private Vector3 playerStartPos;
     private float amountBelowZeroZ;
     private float amountAboveZeroZ;
@@ -19,6 +20,9 @@ public class MixingCamera : MonoBehaviour
     private int xPositionDirection = 1;
     public bool zPositiveDirection;
     public bool xPositiveDirection;
+    public float minWeight;
+    public float maxWeight;
+
 
 
     private CinemachineMixingCamera vcam;
@@ -26,6 +30,7 @@ public class MixingCamera : MonoBehaviour
     
     void Start()
     {
+        
         if (followTarget)
         {
            
@@ -44,16 +49,25 @@ public class MixingCamera : MonoBehaviour
 
     void Update()
     {
+        //playerStartPos = ReferencePos.transform.position;
         playerDetected = cameraTrigger.GetComponent<DetectPlayer>().PlayerInCollider;
 //        Debug.Log(playerDetected);
         if (followTarget)
         {
+            
+            if (vcam.m_Weight1 > 300 || vcam.m_Weight1 < -300)
+            {
+                Debug.LogError("Weight: " + vcam.m_Weight1 + " X Pos: " + followTarget.transform.position.x + " Y Pos: " + followTarget.transform.position.y);
+            }
             if (playerDetected)
             {
                 
                 if (playerStartPos == Vector3.zero)
                 {
                     playerStartPos = followTarget.position;
+                    GameObject marker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                    marker.transform.position = playerStartPos;
+                    marker.GetComponent<Collider>().enabled = false;
                    /* if (followTarget.transform.position.z  0)
                     {
                         playerStartPos.z = playerStartPos.z 
@@ -64,35 +78,52 @@ public class MixingCamera : MonoBehaviour
                     }*/
                     Debug.Log(playerStartPos);
                 }
-            }
-        
-            switch (axisToTrack)
-            {
-                case (AxisEnum.X):
+                switch (axisToTrack)
+                {
+                    case (AxisEnum.X):
                     
-                    vcam.m_Weight1 = Mathf.Abs(followTarget.transform.position.x) - (playerStartPos.x);
-                    vcam.m_Weight1 = vcam.m_Weight1 * xPositionDirection;
-                    break;
-                case (AxisEnum.Z):
-                    vcam.m_Weight1 = Mathf.Abs(followTarget.transform.position.z) - (playerStartPos.z);
-                    vcam.m_Weight1 = vcam.m_Weight1 * zPositionDirection;
-                    break;
-                case (AxisEnum.XZ):
+                        vcam.m_Weight1 = Mathf.Abs(followTarget.transform.position.x) - (playerStartPos.x);
+                        vcam.m_Weight1 = vcam.m_Weight1 * xPositionDirection;
+                        //vcam.m_Weight1 = Mathf.Clamp(vcam.m_Weight1, minWeight, maxWeight);
+                        break;
+                    case (AxisEnum.Z):
+                        vcam.m_Weight1 = Mathf.Abs(followTarget.transform.position.z) - (playerStartPos.z);
                     
-                    float xWeight = Mathf.Abs(followTarget.transform.position.x - playerStartPos.x);
-                    xWeight = xWeight * xPositionDirection;
-                    float zWeight = Mathf.Abs(followTarget.transform.position.z - playerStartPos.z);
-                    zWeight = zWeight * zPositionDirection;
+                        vcam.m_Weight1 = vcam.m_Weight1 * zPositionDirection;
+                        //vcam.m_Weight1 = Mathf.Clamp(vcam.m_Weight1, minWeight, maxWeight);
+                    
+                        break;
+                    case (AxisEnum.XZ):
+                    
+                        float xWeight = Mathf.Abs(followTarget.transform.position.x - playerStartPos.x);
+                        xWeight = xWeight * xPositionDirection;
+                        float zWeight = Mathf.Abs(followTarget.transform.position.z - playerStartPos.z);
+                        zWeight = zWeight * zPositionDirection;
+                        if (followTarget.position.z > playerStartPos.z)
+                        {
+                            zWeight = minWeight;
+                        }
+                        
 //                    Debug.Log(xWeight + "xweight");
-     //               Debug.Log(zWeight + "zweight");
+                        //               Debug.Log(zWeight + "zweight");
 
-                    vcam.m_Weight1 = (xWeight + zWeight) / 2;
+                        vcam.m_Weight1 = (xWeight + zWeight) / 2;
+                        //vcam.m_Weight1 = Mathf.Clamp(vcam.m_Weight1, minWeight, maxWeight);
+                        
                     
                         /*(Mathf.Abs(followTarget.transform.position.x) - (playerStartPos.x))+
                                   (Mathf.Abs(followTarget.transform.position.z) - (playerStartPos.z));
                     vcam.m_Weight1 = vcam.m_Weight1 * zPositionDirection;*/
-                    break;
+                        break;
+                }
             }
+
+            if (playerDetected == false)
+            {
+                playerStartPos = Vector3.zero;
+            }
+        
+            
         }
     }
 }
---
+++ Journey 3.0/Assets/Scripts/TargetSphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSphere : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + _offset;
+    }
+}
