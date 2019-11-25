187dbcb3dcf65e2598ce1bb18c2fda165413f8c3
Samuel Hegner <C16439262@mydit.ie>
2019-11-25 11:24:09 +0000

    GravityChecker Start

---
+++ Journey 3.0/Assets/GravityCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCheck : MonoBehaviour
+{
+    public bool _grounded;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
