a514f115146fc8863303ed3c125882de57b549d3
gazor117 <oreillygary7@gmail.com>
2019-12-03 14:42:28 +0000

    Firefly reference added

---
+++ Journey 3.0/Assets/Scripts/FireflyRef.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyRef : MonoBehaviour
+{
+    private Vector3 _fireFlyRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _fireFlyRef = transform.position;
+        PlayerManager.FireflyRef = _fireFlyRef;
+    }
+}
