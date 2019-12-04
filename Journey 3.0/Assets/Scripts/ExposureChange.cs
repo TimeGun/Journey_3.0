0627da50968ef6790f7cc35bdf8ea9fae22664af
gazor117 <oreillygary7@gmail.com>
2019-12-04 18:58:14 +0000

    Sun Effect test scene added

---
+++ Journey 3.0/Assets/Scripts/ExposureChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExposureChange : MonoBehaviour
+{
+    public float timeChange;
+
+    public float startExposure;
+
+    public float endExposure;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
