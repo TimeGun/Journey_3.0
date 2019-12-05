300e871df20d425a7ae14828d02835e90553a86c
RMBehan <C16424252@mydit.ie>
2019-12-05 11:15:37 +0000

    Fiddling with textures some more

--- Journey 3.0/Assets/RobynTexturing.cs
+++ Journey 3.0/Assets/RobynTexturing.cs
@@ -9,9 +9,28 @@ public class RobynTexturing : MonoBehaviour
     {
         public int textureIndex;
         public int startingHeight;
+        public int overlap;
     }
 
     public SplatHeights[] splatHeights;
+
+    void normalize(float[] v)
+    {
+        float total = 0;
+        for(int i =0; i < v.Length; i++)
+        {
+            total += v[i];
+        }
+        for(int i = 0; i <v.Length; i++)
+        {
+            v[i] /= total;
+        }
+    }
+
+    public float map(float value, float sMin, float sMax, float mMin, float mMax)
+    {
+        return (value - sMin) * (mMax - mMin) / (sMax - sMin) + mMin;
+    }
    
     void Start()
     {
@@ -27,12 +46,21 @@ public class RobynTexturing : MonoBehaviour
 
                 for(int i = 0; i < splatHeights.Length; i++)
                 {
-                    if (i == splatHeights.Length - 1 && terrainHeight >= splatHeights[i].startingHeight)
+                    float thisNoise = Mathf.Clamp(Mathf.PerlinNoise(x * 0.05f, y * 0.05f),0.5f,1f);
+                    float thisHeightStart = splatHeights[i].startingHeight * thisNoise - splatHeights[i].overlap * thisNoise;
+                    float nextHeightStart = 0;
+                    if(i != splatHeights.Length-1)
+                    {
+                        nextHeightStart = splatHeights[i+1].startingHeight * thisNoise + splatHeights[i+1].overlap * thisNoise;
+                    }
+
+                    if (i == splatHeights.Length - 1 && terrainHeight >= thisHeightStart)
                         splat[i] = 1;
-                    else if (terrainHeight >= splatHeights[i].startingHeight && terrainHeight <= splatHeights[i+1].startingHeight)
+                    else if (terrainHeight >= thisHeightStart && terrainHeight <= nextHeightStart)
                         splat[i] = 1;
                 }
 
+                normalize(splat);
                 for(int j =0; j < splatHeights.Length; j++)
                 {
                     splatmapData[x, y, j] = splat[j];
