478875c1d59f7dc171dee0f4adef651d42e9ff57
Samuel Hegner <C16439262@mydit.ie>
2019-12-09 17:58:31 +0000

    Puzzle Playable

--- Journey 3.0/Assets/CheckPlate.cs
+++ Journey 3.0/Assets/CheckPlate.cs
@@ -10,30 +10,60 @@ public class CheckPlate : MonoBehaviour
 
     private bool opened = false;
 
+    [SerializeField] private bool player;
+
     [SerializeField] private UnityEvent openDoor;
+
+    [SerializeField] private Animator _gateAnim;
+    [SerializeField] private Animator _platformAnim;
     
     private void OnTriggerEnter(Collider other)
     {
         PushObject _pushable = other.GetComponent<PushObject>();
 
+        PlayerMovement _movement = other.GetComponent<PlayerMovement>();
+
+        
         if (_pushable != null)
         {
             _boulderPresent = true;
             if (!opened)
             {
+                _pushable.transform.parent = transform.root;
+                Destroy(_pushable);
+                
                 openDoor.Invoke();
                 opened = true;
             }
         }
+        
+        if (_movement != null)
+        {
+            player = true;
+        }
+    }
+
+    private void Update()
+    {
+        _gateAnim.SetBool("playerWeight", player);
+        _platformAnim.SetBool("playerWeight", player);
     }
 
     private void OnTriggerExit(Collider other)
     {
         PushObject _pushable = other.GetComponent<PushObject>();
 
+        PlayerMovement _movement = other.GetComponent<PlayerMovement>();
+        
+
         if (_pushable != null)
         {
             _boulderPresent = false;
+        }
+
+        if (_movement != null)
+        {
+            player = false;
         }
     }
 }
