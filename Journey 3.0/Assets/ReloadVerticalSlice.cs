42f91e1e93c38993f8203747c791c61edb515976
Samuel Hegner <C16439262@mydit.ie>
2019-12-10 20:24:20 +0000

    Scene Reloading

---
+++ Journey 3.0/Assets/ReloadVerticalSlice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadVerticalSlice : MonoBehaviour
+{
+    [SerializeField] private SceneBundleSO sceneBundle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+
+
+        }
+    }
+
+    
+}
