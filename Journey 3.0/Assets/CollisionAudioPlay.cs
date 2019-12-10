2c5f5d4e8d3f28d233ef076a7be9a8c52939c0f5
Samuel Hegner <C16439262@mydit.ie>
2019-12-10 19:51:47 +0000

    Collision of Basket Sound

--- Journey 3.0/Assets/CollisionAudioPlay.cs
+++ Journey 3.0/Assets/CollisionAudioPlay.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionAudioPlay : MonoBehaviour
 {
+    public AudioSource _source;
+    public AudioClip _clip;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.layer == 8)
+        {
+            _source.PlayOneShot(_clip);
+            Destroy(this);
+        }
     }
+
+    
 }
