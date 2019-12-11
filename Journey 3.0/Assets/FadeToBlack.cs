cc71ecfbc90f1c3f3ae019b7e7a7e0000562d822
samuelhegner <C16439262@mydit.ie>
2019-12-11 03:13:13 +0000

    Fade to black added

---
+++ Journey 3.0/Assets/FadeToBlack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeToBlack : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+
+    public static FadeToBlack instance;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void SetBars(bool value)
+    {
+        _anim.SetBool("blackBarsOn", value);
+    }
+}
