9347f57479d8d7f055dd23313e50000acdd75281
Samuel Hegner <C16439262@mydit.ie>
2020-02-07 10:22:58 +0000

    Things and stuff

--- Journey 3.0/Assets/Scripts/SceneBundleSO.cs
+++ Journey 3.0/Assets/Scripts/SceneBundleSO.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+
 
 
 [CreateAssetMenu(fileName = "New Scene Bundle", menuName = "SceneBundle")]
