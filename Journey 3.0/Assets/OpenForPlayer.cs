3e92fca8b07b0bd80b5540139e94d056376a287f
Samuel Hegner <C16439262@mydit.ie>
2020-02-14 15:14:00 +0000

    Floating Rune

--- Journey 3.0/Assets/OpenForPlayer.cs
+++ Journey 3.0/Assets/OpenForPlayer.cs
@@ -8,13 +8,58 @@ public class OpenForPlayer : MonoBehaviour
 
     private GameObject player;
 
+    [SerializeField] private float _hoverDistance = 2f;
+
+    [SerializeField] private float _activationDistance = 5f;
+
+    private float _lerpSpeed;
+    
+    [SerializeField] private float _lerpSpeedMax;
+
+    [SerializeField] private bool testing;
+
+    private Vector3 _closedRunePosition;
+
     void Start()
     {
-        
+        if (testing)
+        {
+            player = GameObject.Find("Player");
+        }
+        else
+        {
+            player = API.GlobalReferences.PlayerRef;
+        }
+
+        _closedRunePosition = _topRunePart.transform.position;
     }
 
     void Update()
     {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        print(_lerpSpeed);
+        if (distanceToPlayer < _activationDistance)
+        {
+            _lerpSpeed = Map(distanceToPlayer, _activationDistance, 0, 0, _lerpSpeedMax);
+            float tempHoverDistance = Map(distanceToPlayer, _activationDistance, 0, 0, _hoverDistance);
+            _topRunePart.transform.position = 
+                Vector3.Lerp(_topRunePart.transform.position, _closedRunePosition + new Vector3(0, tempHoverDistance, 0), Time.deltaTime * _lerpSpeed);
+        }
+        else
+        {
+            _lerpSpeed = _lerpSpeedMax;
+            _topRunePart.transform.position = Vector3.MoveTowards(_topRunePart.transform.position, _closedRunePosition, Time.deltaTime * _lerpSpeed * 2f);
+        }
+    }
+    
+    public float Map(float a, float b, float c, float d, float e)
+    {
         
+        float cb = c - b;
+        float de = e - d;
+        float howFar = (a - b) / cb;
+        return d + howFar * de;
+        
+        //float a = value you want mapped t
     }
 }
