5f5271f537c93c2a0b17256794b53c64d17e51f0
Samuel Hegner <C16439262@mydit.ie>
2020-02-14 14:08:30 +0000

    Start of new Rune

---
+++ Journey 3.0/Assets/OpenForPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenForPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject _topRunePart;
+
+    private GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
