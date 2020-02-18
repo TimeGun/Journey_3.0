e2feb14c1189365b708a023ec491031093bc1602
samuelhegner <C16439262@mydit.ie>
2020-02-18 18:06:20 +0000

    Plank placing start

---
+++ Journey 3.0/Assets/PlankPlacement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject _centerOfGapObj;
+
+    private bool plankIsPlaceDown;
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
