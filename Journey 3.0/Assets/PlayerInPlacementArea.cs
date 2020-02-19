623e1bf7d8a3a8579cf88e816cf9f43c8a6c042a
Samuel Hegner <C16439262@mydit.ie>
2020-02-19 11:37:45 +0000

    PlayerInPlacementArea Trigger working

--- Journey 3.0/Assets/PlayerInPlacementArea.cs
+++ Journey 3.0/Assets/PlayerInPlacementArea.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerInPlacementArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private bool _playerInTrigger;
 
-    // Update is called once per frame
-    void Update()
+    public bool PlayerInTrigger
     {
-        
+        get => _playerInTrigger;
+        set => _playerInTrigger = value;
     }
 }
---
+++ Journey 3.0/Assets/PlayerPresentInTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPresentInTrigger : MonoBehaviour
+{
+    [SerializeField] private PlayerInPlacementArea _playerInPlacementArea;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInPlacementArea.PlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInPlacementArea.PlayerInTrigger = false;
+        }
+    }
+}
