1a682f9f386c0164071c2c08c1c6f9f43057672c
Samuel Hegner <C16439262@mydit.ie>
2020-02-21 10:28:29 +0000

    Teleport player prefab start

--- Journey 3.0/Assets/TeleportPlayer.cs
+++ Journey 3.0/Assets/TeleportPlayer.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
+
+    [SerializeField] private GameObject _teleportExitLocation;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,29 @@ public class TeleportPlayer : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TeleportToExit(other.transform.root.gameObject);
+        }
+    }
+
+    private void TeleportToExit(GameObject objectToTeleport)
+    {
+        objectToTeleport.transform.position = _teleportExitLocation.transform.position;
+        print("called");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0, 1f, 0.4f);
+        Gizmos.DrawSphere(_teleportExitLocation.transform.position, 10f);
+        
+        Gizmos.color = new Color(1, 0, 1f, 0.4f);
+        Gizmos.DrawSphere(transform.position, 10f);
     }
 }
