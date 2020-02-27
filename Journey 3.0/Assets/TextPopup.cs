9c7ba73089f759fae98501cd79ebdc63714af9ec
Samuel Hegner <C16439262@mydit.ie>
2020-02-27 11:49:57 +0000

    Text Popup Prefab (Prefab folder)

---
+++ Journey 3.0/Assets/TextPopup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+
+public class TextPopup : MonoBehaviour
+{
+    public static TextPopup instance;
+
+    [SerializeField] private TMP_Text textUI;
+
+    [SerializeField] private float timeToDisplayText;
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    public IEnumerator DisplayText(string textToDisplay)
+    {
+        textUI.text = textToDisplay;
+        
+        yield return new WaitForSeconds(timeToDisplayText);
+
+        textUI.text = "";
+    }
+}
---
+++ Journey 3.0/Assets/TriggerTextPopup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTextPopup : MonoBehaviour
+{
+    [SerializeField] private string textToDisplay;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            TextPopup.instance.StartCoroutine(TextPopup.instance.DisplayText(textToDisplay));
+        }
+    }
+}
