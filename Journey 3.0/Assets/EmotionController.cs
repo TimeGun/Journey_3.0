55429b642bac35ba8af584a8cd9259eb8842514a
samuelhegner <C16439262@mydit.ie>
2020-03-09 22:09:52 +0000

    Timeline working with emotion changer

---
+++ Journey 3.0/Assets/EmotionController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionController : MonoBehaviour
+{
+
+    [SerializeField] private Color _colour;
+    [SerializeField] private float _emissionIntensity;
+
+    [SerializeField] private Material _material;
+
+    [SerializeField] private float _emissionIntensityLerpSpeed;
+    [SerializeField] private float _colourLerpSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AdjustEmotionSettings());
+    }
+
+    public void SetColour(Color col)
+    {
+        _colour = col;
+    }
+
+    public void SetEmissionIntensity(float intensity)
+    {
+        _emissionIntensity = intensity;
+    }
+
+    private IEnumerator AdjustEmotionSettings()
+    {
+        while (true)
+        {
+            if (_material.GetColor("_emissionColour") != _colour)
+            {
+                _material.SetColor("_emissionColour", Color.Lerp(_material.GetColor("_emissionColour"), _colour, _colourLerpSpeed * Time.deltaTime));
+            }
+
+            if (_material.GetFloat("_emissionIntensity") != _emissionIntensity)
+            {
+                _material.SetFloat("_emissionIntensity", Mathf.Lerp(_material.GetFloat("_emissionIntensity"), _emissionIntensity, _emissionIntensityLerpSpeed * Time.deltaTime));
+            }
+            
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
