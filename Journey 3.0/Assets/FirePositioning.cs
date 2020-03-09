49244c4192635cb712a8e1c887a4ae64837d5943
Patnjop <36369847+Patnjop@users.noreply.github.com>
2020-03-09 19:13:39 +0000

    VFX Update
    -Lightning
    -Rune Surrounding
    -Test Fire target

---
+++ Journey 3.0/Assets/FirePositioning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class FirePositioning : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect visualEffect;
+
+    [SerializeField]
+    private Vector3 fireTargetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        visualEffect.SetVector3("Fire Pos", fireTargetPos);
+    }
+}
