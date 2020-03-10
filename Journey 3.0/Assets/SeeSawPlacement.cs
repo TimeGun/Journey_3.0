c460a905961241ea04ceb467fd898892b5d74bd6
samuelhegner <C16439262@mydit.ie>
2020-03-10 17:33:02 +0000

    Place rock  on egg saw

---
+++ Journey 3.0/Assets/SeeSawPlacement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeSawPlacement : MonoBehaviour, IPlaceableArea
+{
+    [SerializeField] private GameObject _boulder;
+
+    [SerializeField] private GameObject _boulderPlacement;
+
+    [SerializeField] private bool _boulderPlacedDown = false;
+
+    [SerializeField] private bool playerInThisTrigger = false;
+
+    public bool PlayerInThisTrigger
+    {
+        get => playerInThisTrigger;
+        set => playerInThisTrigger = value;
+    }
+
+    public GameObject GetCenterObject()
+    {
+        return _boulderPlacement;
+    }
+
+    public GameObject GetPlank()
+    {
+        return _boulder;
+    }
+
+    public void SetPlank(GameObject value)
+    {
+        _boulder = value;
+    }
+
+    public bool GetPlankPlacedDown()
+    {
+        return _boulderPlacedDown;
+    }
+
+    public void SetPlankPlacedDown(bool value)
+    {
+        _boulderPlacedDown = value;
+    }
+
+    public bool AdjustPositionBool()
+    {
+        return false;
+    }
+}
