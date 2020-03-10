1e31920c57cecb720e8b1fcdd98f3c793c59db28
samuelhegner <C16439262@mydit.ie>
2020-03-10 19:27:57 +0000

    Seesaw (needs raycast)

---
+++ Journey 3.0/Assets/SeeSawAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeSawAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        _anim.SetBool("PlayerPresent", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _anim.SetBool("PlayerPresent", false);
+    }
+}
