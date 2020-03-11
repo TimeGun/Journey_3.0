f53ce67f59f7baf71aace75ff3597311f48fdf76
Samuel Hegner <C16439262@mydit.ie>
2020-03-11 16:10:08 +0000

    Cast Fix

--- Journey 3.0/Assets/GroundRayCast.cs
+++ Journey 3.0/Assets/GroundRayCast.cs
@@ -7,11 +7,16 @@ public class GroundRayCast : MonoBehaviour
     [SerializeField] private LayerMask _mask;
 
     [SerializeField] private float raycastsPerSecond;
+
+
+    [SerializeField] private bool _standingOnSeesaw;
     
+    private SeeSawAnimation _seeSawAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(CheckFloor());
     }
 
     // Update is called once per frame
@@ -25,7 +30,34 @@ public class GroundRayCast : MonoBehaviour
     {
         while (true)
         {
-            Physics.Raycast()
+            RaycastHit hit;
+
+            
+            Ray ray = new Ray(transform.position, Vector3.down);
+            
+            Debug.DrawLine(transform.position, transform.position + (Vector3.down * 0.2f),  Color.red);
+
+            if (Physics.Raycast(ray, out hit, 0.2f, _mask))
+            {
+                if (hit.transform.gameObject.layer == 13 && _seeSawAnimation == null)
+                {
+                    _seeSawAnimation = hit.transform.GetComponentInParent<SeeSawAnimation>();
+                }
+            }
+            else
+            {
+                if (_seeSawAnimation != null)
+                {
+                    _seeSawAnimation.SetAnimationBoolS("PlayerPresent", false);
+                    _seeSawAnimation = null;
+                }
+            }
+
+            if (_seeSawAnimation != null)
+            {
+                _seeSawAnimation.SetAnimationBoolS("PlayerPresent", true);
+            }
+
             yield return new WaitForSeconds(1/raycastsPerSecond);
         }
     }
--- Journey 3.0/Assets/SeeSawAnimation.cs
+++ Journey 3.0/Assets/SeeSawAnimation.cs
@@ -6,5 +6,10 @@ using UnityEngine;
 public class SeeSawAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
-    
+
+
+    public void SetAnimationBoolS(string boolName, bool value)
+    {
+        _anim.SetBool(boolName, value);
+    }
 }
