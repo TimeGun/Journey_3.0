e31e1745beb3129ce1eb1dea8fb237282f792692
samuelhegner <C16439262@mydit.ie>
2020-03-16 17:29:59 +0000

    Lighting Spawn Start

--- Journey 3.0/Assets/SpawnLightning.cs
+++ Journey 3.0/Assets/SpawnLightning.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnLightning : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _player;
+
+    [SerializeField] private GameObject _lightningPrefab;
+    
+    [SerializeField] private float _timeBetweenStrike;
+
+    private Coroutine coroutine;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            coroutine = StartCoroutine(SpawnLightningPrefab());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        StopCoroutine(coroutine);
+    }
+
+    public void StartLightningBattle()
+    {
+        StartCoroutine(SpawnLightningPrefab());
+    }
+
+    IEnumerator SpawnLightningPrefab()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_timeBetweenStrike);
+
+            Instantiate(_lightningPrefab, _player.transform.position, transform.rotation);
+        }
     }
 }
