9a046abcee30d6320118f1727f5c9ca0b933e822
samuelhegner <C16439262@mydit.ie>
2020-03-16 14:25:50 +0000

    Kockback Added

---
+++ Journey 3.0/Assets/LightningStrike.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class LightningStrike : MonoBehaviour
+{
+    
+    //spawn on player
+    // show the preemtive strike 
+    //strike
+    //check if the player was hit
+    //normal of the mesh for rigidbody force
+    //push player down the slope
+
+    [SerializeField] private float timeBeforeStrike;
+
+    [SerializeField] private float forceToPushPlayer;
+
+    private Vector3 pushDirection;
+
+    [SerializeField] private VisualEffect preStrikeVFX;
+
+    [SerializeField] private VisualEffect strikeVFX;
+
+    [SerializeField] private bool playerInHitbox = false;
+
+    private GameObject player;
+
+    [SerializeField] private LayerMask mask; 
+    
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        StartCoroutine(StrikeCoroutine());
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            playerInHitbox = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInHitbox = false;
+        }
+    }
+    IEnumerator StrikeCoroutine()
+    {
+        preStrikeVFX.enabled = true;
+        yield return new WaitForSeconds(timeBeforeStrike);
+        preStrikeVFX.Stop();
+        strikeVFX.enabled = true;
+        strikeVFX.Play();
+
+        if (playerInHitbox)
+        {
+            Ray ray = new Ray(transform.position, Vector3.down);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast( ray, out hit, 2f, mask))
+            {
+                pushDirection = Vector3.Cross(hit.normal, transform.right);
+                print(pushDirection);
+            }
+            
+            player.GetComponent<ReceiveImpact>().AddImpact(pushDirection , forceToPushPlayer);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(pushDirection != Vector3.zero)
+            Gizmos.DrawLine(transform.position, transform.position + (pushDirection * 10f));
+    }
+}
---
+++ Journey 3.0/Assets/ReceiveImpact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveImpact : MonoBehaviour
+{
+    float mass = 3.0f; // defines the character mass
+    Vector3 impact = Vector3.zero;
+    
+    private CharacterController _characterController;
+ 
+    void Start(){
+        _characterController = GetComponent<CharacterController>();
+    }
+ 
+    // call this function to add an impact force:
+    public void AddImpact(Vector3 direction, float forceToApply){
+        direction.Normalize();
+        if (direction.y < 0) direction.y = -direction.y; // reflect down force on the ground
+        impact += direction.normalized * forceToApply / mass;
+    }
+ 
+    void Update(){
+        // apply the impact force:
+        if (impact.magnitude > 0.2) _characterController.Move(impact * Time.deltaTime);
+        // consumes the impact energy each cycle:
+        impact = Vector3.Lerp(impact, Vector3.zero, 5*Time.deltaTime);
+    }
+}
