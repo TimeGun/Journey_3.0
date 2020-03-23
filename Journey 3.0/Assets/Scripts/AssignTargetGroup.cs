8bcc7f55cd0d8e67d7860c8476c1ddc793b43ad9
gazor117 <oreillygary7@gmail.com>
2020-03-23 16:15:51 +0000

    Cameras ready for multi scene

---
+++ Journey 3.0/Assets/Scripts/AssignTargetGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class AssignTargetGroup : MonoBehaviour
+{
+    public CinemachineTargetGroup TG;
+    
+
+    public bool assignPlayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TG = GetComponent<CinemachineTargetGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (assignPlayer)
+        {
+            TG.AddMember(API.GlobalReferences.PlayerRef.transform, 1, 0);
+            TG.RemoveMember(null);
+            
+        }
+    }
+}
