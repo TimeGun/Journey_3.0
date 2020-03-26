5ab2332752c57942b6ea76ffc5056dd38f145ac5
samuelhegner <C16439262@mydit.ie>
2020-03-26 16:03:45 +0000

    elevator ready for level design

---
+++ Journey 3.0/Assets/SetElevatorCogSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetElevatorCogSpeed : MonoBehaviour
+{
+    [SerializeField] private ElevatorMovement _elevatorMovement;
+
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float lastSpeed = 0;
+    
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (_elevatorMovement.ElevatorMovementSpeed != lastSpeed)
+        {
+            SetNewSpeed(_elevatorMovement.ElevatorMovementSpeed);
+            lastSpeed = _elevatorMovement.ElevatorMovementSpeed;
+        }
+    }
+
+    void SetNewSpeed(float newSpeed)
+    {
+        _animator.SetFloat("SpinningSpeed", newSpeed);
+    }
+}
