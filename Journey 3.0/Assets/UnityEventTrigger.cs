44dcf1ce9e95483dd95364d326ccd52e76e97dcc
samuelhegner <C16439262@mydit.ie>
2020-03-26 15:00:02 +0000

    Elevator Temp Textures and Work

---
+++ Journey 3.0/Assets/UnityEventTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _unityEventToTrigger;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _unityEventToTrigger.Invoke();
+        }
+    }
+}
