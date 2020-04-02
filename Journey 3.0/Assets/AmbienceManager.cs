edf9cde590689dd8c74444db3029bb55e14196c4
Samuel Hegner <C16439262@mydit.ie>
2020-04-02 17:13:34 +0100

    Ambience Audio Setup

---
+++ Journey 3.0/Assets/AmbienceManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceManager : MonoBehaviour
+{
+    public static AmbienceManager instance;
+
+
+    [SerializeField] private AudioSource[] _outsideSource;
+
+    [SerializeField] private AudioSource[] _insideSource;
+
+    [SerializeField] private AudioSource[] _topOfTheMountainSources;
+
+    [SerializeField] private float moveTowardsSpeed;
+
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void SetOutSideAmbience(float newVolume)
+    {
+        StartCoroutine(SetNewVolumes(_outsideSource, newVolume));
+    }
+
+    public void SetInsideAmbience(float newVolume)
+    {
+        StartCoroutine(SetNewVolumes(_insideSource, newVolume));
+
+    }
+
+    public void SetTopOfTheMountainAmbience(float newVolume)
+    {
+        StartCoroutine(SetNewVolumes(_topOfTheMountainSources, newVolume));
+    }
+
+    IEnumerator SetNewVolumes(AudioSource[] sources, float newVolume)
+    {
+        
+        
+        if (sources == null)
+            yield break;
+        
+        if (!sources[0].isPlaying)
+        {
+            for (int i = 0; i < sources.Length; i++)
+            {
+                sources[i].Play();
+            }
+        }
+        
+        
+        while (sources[0].volume != newVolume)
+        {
+            for (int i = 0; i < sources.Length; i++)
+            {
+                MoveVolumeTo(sources[i], newVolume);
+            }
+            
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (newVolume == 0)
+        {
+            for (int i = 0; i < sources.Length; i++)
+            {
+                sources[i].Stop();
+            }
+        }
+    }
+    
+    
+    void MoveVolumeTo(AudioSource source, float targetVolume)
+    {
+        source.volume = Mathf.MoveTowards(source.volume, targetVolume, Time.deltaTime * moveTowardsSpeed);
+    }
+}
---
+++ Journey 3.0/Assets/AmbientAudioTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AmbientAudioTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _event;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _event.Invoke();
+        }
+    }
+
+    public void SetOutSideAmbience(float newVolume)
+    {
+        AmbienceManager.instance.SetOutSideAmbience(newVolume);
+    }
+
+    public void SetInsideAmbience(float newVolume)
+    {
+        AmbienceManager.instance.SetInsideAmbience(newVolume);
+
+    }
+
+    public void SetMountainTopAmbience(float newVolume)
+    {
+        AmbienceManager.instance.SetTopOfTheMountainAmbience(newVolume);
+
+    }
+}
