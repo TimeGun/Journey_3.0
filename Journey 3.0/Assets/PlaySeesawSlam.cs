81ad007aa643d6c7eb4fbeba25e0236a64f1587c
Samuel Hegner <C16439262@mydit.ie>
2020-04-02 16:03:31 +0100

    Seesaw Audio

---
+++ Journey 3.0/Assets/PlaySeesawSlam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySeesawSlam : MonoBehaviour
+{
+    
+    [SerializeField] private AudioSource _slamAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SeesawTip"))
+        {
+            print("slam time");
+            _slamAudio.PlayOneShot(_slamAudio.clip);
+        }
+    }
+}
---
+++ Journey 3.0/Assets/SeesawAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeesawAudio : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource _creakSound;
+
+    [SerializeField] private Animator _animator;
+
+    bool tipped;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Creak"))
+        {
+            if (!_creakSound.isPlaying)
+            {
+                _creakSound.PlayOneShot(_creakSound.clip);
+            }
+        }
+        else
+        {
+            _creakSound.Stop();
+        }
+    }
+}
