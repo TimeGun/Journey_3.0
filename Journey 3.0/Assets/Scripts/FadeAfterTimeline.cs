fca406be9464df8e4b6f9ab9435ea7154a7c2a15
gazor117 <oreillygary7@gmail.com>
2020-04-03 14:13:44 +0100

    Restarts game and fades to black

---
+++ Journey 3.0/Assets/Scripts/FadeAfterTimeline.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class FadeAfterTimeline : MonoBehaviour
+{
+    
+    public PlayableDirector playableDirector;
+    public GameObject cameraTrigger;
+    private bool _playerEntered;
+    private double _timelineLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timelineLength = playableDirector.duration;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (cameraTrigger != null)
+        {
+            _playerEntered = cameraTrigger.GetComponent<DetectPlayer>().PlayerEntered;    //Use a coroutine to check if the player has entered every x amount of frames
+
+        }
+
+        if (_playerEntered)
+        {
+            StartCoroutine(FadeToBlack());
+        }
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        yield return new WaitForSeconds((float)_timelineLength);
+        Debug.Log("FADE OUT");
+        global::FadeToBlack.instance.SetBlack(true);
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("Manager Scene");
+    }
+}
