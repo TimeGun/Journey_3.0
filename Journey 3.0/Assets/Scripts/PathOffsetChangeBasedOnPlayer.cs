3ddc7ec18a33774319179953c449e938b9354533
gazor117 <oreillygary7@gmail.com>
2020-04-12 18:32:55 +0100

    Offset Change based on Glyf Direction

---
+++ Journey 3.0/Assets/Scripts/PathOffsetChangeBasedOnPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PathOffsetChangeBasedOnPlayer : MonoBehaviour
+{
+
+    private Vector3 playerForward;
+    public GameObject targetCamera;
+    public GameObject player;
+    public float startOffset;
+    public float endOffset;
+    private float adjustedOffset;
+    public int checksPerSecond;
+    private CinemachineVirtualCamera cameraBase;
+    private float angleBetweenPlayerAndCamera;
+    public bool testScene;
+    private CinemachineTrackedDolly TD;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraBase = targetCamera.GetComponent<CinemachineVirtualCamera>();
+        TD = cameraBase.GetCinemachineComponent<CinemachineTrackedDolly>();
+        StartCoroutine(CheckAngle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (testScene != true)
+        {
+            player = API.GlobalReferences.PlayerRef;            
+        }
+        
+    }
+
+
+
+    IEnumerator CheckAngle()
+    {
+        
+        if (targetCamera.activeSelf)
+        {
+            yield return new WaitForSeconds(Time.deltaTime);
+            while (true)
+            {
+                angleBetweenPlayerAndCamera = Vector3.Angle(player.transform.forward,
+                    targetCamera.transform.forward);
+                if (angleBetweenPlayerAndCamera >= 90f)
+                {
+                    adjustedOffset = endOffset;
+                }
+                else
+                {
+                    adjustedOffset = startOffset;
+                }
+                Debug.Log(adjustedOffset);
+                TD.m_AutoDolly.m_PositionOffset = adjustedOffset;
+                
+                yield return new WaitForSeconds(1f / checksPerSecond);
+
+            }
+        }
+    }
+}
