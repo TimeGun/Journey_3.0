15be8d6134bcd225c1b30c43a1082f2ea344c76d
Samuel Hegner <C16439262@mydit.ie>
2020-04-13 21:34:26 +0100

    Lerping Skybox Finished

--- Journey 3.0/Assets/LerpDayToNight.cs
+++ Journey 3.0/Assets/LerpDayToNight.cs
@@ -1,51 +1,44 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 
 public class LerpDayToNight : MonoBehaviour
 {
-    [SerializeField] private VolumeProfile _skyboxProfile;
-    [SerializeField] private VolumeProfile _nightTimeSkyboxProfine;
+    [SerializeField] private VolumeProfile _dayTimeSkyboxProfile;
+    [SerializeField] private VolumeProfile _nightTimeSkyboxProfile;
+    [SerializeField] private VolumeProfile _changingSkyboxProfile;
 
-    [SerializeField] private GradientSky _skybox;
+    [SerializeField] private GradientSky _daySkybox;
     [SerializeField] private GradientSky _nightSkybox;
+    [SerializeField] private GradientSky _changingSkybox;
 
     [SerializeField] private float lerpTimer;
 
 
-    private ColorParameter _originalTop;
-    private ColorParameter _originalMiddle;
-    private ColorParameter _originalBottom;
-    
-    
     // Start is called before the first frame update
     void Start()
     {
+        EditorApplication.playmodeStateChanged += ModeChanged;
+        _dayTimeSkyboxProfile.TryGet(out _daySkybox);
+        _nightTimeSkyboxProfile.TryGet(out _nightSkybox);
+
+
+        _changingSkyboxProfile.TryGet(out _changingSkybox);
+
+
+        _changingSkybox.bottom.value = _daySkybox.bottom.value;
+        _changingSkybox.middle.value = _daySkybox.middle.value;
+        _changingSkybox.top.value = _daySkybox.top.value;
         
-        _skyboxProfile.TryGet(out _skybox);
-        _nightTimeSkyboxProfine.TryGet(out _nightSkybox);
-
-        _originalBottom = _skybox.bottom;
-        _originalMiddle = _skybox.middle;
-        _originalTop = _skybox.top;
-
-        //_skybox.bottom.value = _nightSkybox.bottom.value;
-        //_skybox.middle.value = _nightSkybox.middle.value;
-        //_skybox.top.value = _nightSkybox.top.value;
 
         StartCoroutine(ChangeDayToNight(lerpTimer));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-   
 
     private IEnumerator ChangeDayToNight(float timeToLerpFor)
     {
@@ -54,12 +47,14 @@ public class LerpDayToNight : MonoBehaviour
         
         while (timeLeft > 0)
         {
-            _skybox.gradientDiffusion.value = Mathf.Lerp(1.3f, 1f, Time.deltaTime / timeLeft);
+            
+            print("gas");
+            _changingSkybox.gradientDiffusion.value = Mathf.Lerp(_changingSkybox.gradientDiffusion.value, _nightSkybox.gradientDiffusion.value, Time.deltaTime / timeLeft);
 
 
-            _skybox.bottom.value = Color.Lerp(_skybox.bottom.value, _nightSkybox.bottom.value, Time.deltaTime/timeLeft);
-            _skybox.middle.value = Color.Lerp(_skybox.middle.value, _nightSkybox.middle.value, Time.deltaTime/timeLeft);
-            _skybox.top.value = Color.Lerp(_skybox.top.value, _nightSkybox.top.value, Time.deltaTime/timeLeft);
+            _changingSkybox.bottom.value = Color.Lerp(_changingSkybox.bottom.value, _nightSkybox.bottom.value, Time.deltaTime/timeLeft);
+            _changingSkybox.middle.value = Color.Lerp(_changingSkybox.middle.value, _nightSkybox.middle.value, Time.deltaTime/timeLeft);
+            _changingSkybox.top.value = Color.Lerp(_changingSkybox.top.value, _nightSkybox.top.value, Time.deltaTime/timeLeft);
 
 
             timeLeft -= Time.deltaTime;
@@ -67,12 +62,16 @@ public class LerpDayToNight : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
     }
-
-
-    private void OnDestroy()
+    
+ 
+    void ModeChanged ()
     {
-        _skybox.bottom.SetValue(_originalBottom);
-        _skybox.middle.SetValue(_originalMiddle);
-        _skybox.top.SetValue(_originalTop);
+        if (!EditorApplication.isPlayingOrWillChangePlaymode &&
+            EditorApplication.isPlaying ) 
+        {
+            _changingSkybox.bottom.value = _daySkybox.bottom.value;
+            _changingSkybox.middle.value = _daySkybox.middle.value;
+            _changingSkybox.top.value = _daySkybox.top.value;
+        }
     }
 }
