f90b77fb06a3b0b47148118af9c35ebf9fc05489
Samuel Hegner <C16439262@mydit.ie>
2020-04-14 12:08:25 +0100

    SpawnPoints Moved and Save system/menu start

--- Journey 3.0/Assets/SaveSystem.cs
+++ Journey 3.0/Assets/SaveSystem.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-public class SaveSystem : MonoBehaviour
+public static class SaveSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    private static string path = Application.persistentDataPath + "/player.progress";
+    
+    public static void SaveProgress(ProgressionData progressionData)
     {
+        BinaryFormatter formatter = new BinaryFormatter();
         
+        FileStream stream = new FileStream(path, FileMode.Create);
+        
+        formatter.Serialize(stream, progressionData);
+        
+        stream.Close();
     }
 
-    // Update is called once per frame
-    void Update()
+    public static ProgressionData LoadProgressionData()
     {
-        
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            ProgressionData progressionData = formatter.Deserialize(stream) as ProgressionData;
+            
+            stream.Close();
+
+            return progressionData;
+        }
+        else
+        {
+            Debug.LogError("No save file found at: " + path);
+            return null;
+        }
+    }
+}
+
+[System.Serializable]
+public class ProgressionData
+{
+    public int saveSectionIndex;
+
+    public bool nightTime;
+
+    public ProgressionData(int tempSaveSectionIndex, bool tempNightTime)
+    {
+        saveSectionIndex = tempSaveSectionIndex;
+
+        nightTime = tempNightTime;
     }
 }
---
+++ Journey 3.0/Assets/StartingZones.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingZones : MonoBehaviour
+{
+    [SerializeField] private Transform[] _debugStartingLocations;
+    
+    [SerializeField] private Transform[] _startingLocations;
+
+    private int sectionIndex;
+    
+}
