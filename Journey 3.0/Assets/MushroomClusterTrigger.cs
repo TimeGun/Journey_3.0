6c61084bbe36666da191220202d1f20931b1e2c0
Samuel Hegner <C16439262@mydit.ie>
2020-04-16 14:50:01 +0100

    Mushroom Trigger Start

---
+++ Journey 3.0/Assets/MushroomClusterTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomClusterTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator[] _mushroomAnimators;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            ShootSpores();
+        }
+    }
+
+    void ShootSpores()
+    {
+        for (int i = 0; i < _mushroomAnimators.Length; i++)
+        {
+            _mushroomAnimators[i].Play("MovingMushroom");
+        }
+    }
+}
