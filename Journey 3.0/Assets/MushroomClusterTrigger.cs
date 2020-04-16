debfbe8b765adfe4034a9e7fe07a5e1a3f80af40
Samuel Hegner <C16439262@mydit.ie>
2020-04-16 15:04:33 +0100

    Interactible Mushroom Prefabs

--- Journey 3.0/Assets/MushroomClusterTrigger.cs
+++ Journey 3.0/Assets/MushroomClusterTrigger.cs
@@ -2,17 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class MushroomClusterTrigger : MonoBehaviour
 {
     [SerializeField] private Animator[] _mushroomAnimators;
 
+    [SerializeField] private VisualEffect _spores;
+
+    private float timer;
+
+    [SerializeField] private float sporeTimer;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
-            ShootSpores();
+            if(timer <= 0)
+                ShootSpores();
         }
+    }
+
+    private void Update()
+    {
+        if(timer > 0)
+            timer -= Time.deltaTime;
     }
 
     void ShootSpores()
@@ -21,5 +35,7 @@ public class MushroomClusterTrigger : MonoBehaviour
         {
             _mushroomAnimators[i].Play("MovingMushroom");
         }
+        _spores.SendEvent("Burst");
+        timer = sporeTimer;
     }
 }
