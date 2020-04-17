129d3ad8a841d3056b48b082784e14962d385500
Samuel Hegner <C16439262@mydit.ie>
2020-04-17 12:39:30 +0100

    Rope Start

---
+++ Journey 3.0/Assets/HitGroundKinematic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HitGroundKinematic : MonoBehaviour
+{
+    private Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            _rb.isKinematic = true;
+            Destroy(this);
+        }
+    }
+}
