0ea608dec06ad3bd13a14b507d2bed4d508a6ddc
Samuel Hegner <C16439262@mydit.ie>
2020-04-23 14:55:59 +0100

    Analytics Start

--- Journey 3.0/Assets/AverageFramesPerSecond.cs
+++ Journey 3.0/Assets/AverageFramesPerSecond.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class AverageFramesPerSecond : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static float GetAverageFrameRate()
     {
-        
-    }
+        float avg = Time.frameCount / Time.time;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return avg;
     }
 }
