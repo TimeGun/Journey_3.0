ffad56a6c5e6ab5b1292ffa57f82d157e37b6ac5
Samuel Hegner <C16439262@mydit.ie>
2020-04-29 17:20:20 +0100

    Torch beast mode and Menu Fix

---
+++ Journey 3.0/Assets/TorchMovementModifier.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class TorchMovementModifier : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    [SerializeField] private VisualEffect _torch;
+
+    private bool _moving;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GameObject.Find("Player").GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_characterController.velocity.magnitude > 0.25f)
+        {
+            if (!_moving)
+            {
+                _moving = true;
+                _torch.SetBool("IsMoving", _moving);
+            }
+
+            
+        }
+        else
+        {
+            if (_moving)
+            {
+                _moving = false;
+                _torch.SetBool("IsMoving", _moving);
+            }
+        }
+    }
+}
