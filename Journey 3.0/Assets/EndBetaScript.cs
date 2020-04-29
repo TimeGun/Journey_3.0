f1a72c228398ae100c8f463cfa04fbe6f1f21ad4
Samuel Hegner <C16439262@mydit.ie>
2020-04-29 13:04:48 +0100

    End Beta Screen Implemented

---
+++ Journey 3.0/Assets/EndBetaScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EndBetaScript : MonoBehaviour
+{
+    [SerializeField] private GameObject steam;
+
+    [SerializeField] private MenuController _menuController;
+
+    public void EnableBateScreen()
+    {
+        _menuController.enabled = false;
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(steam);
+    }
+}
---
+++ Journey 3.0/Assets/OpenHyperLink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenHyperLink : MonoBehaviour
+{
+    public string hyperLink;
+    
+    public void OpenLink()
+    {
+        Application.OpenURL(hyperLink);
+    }
+}
