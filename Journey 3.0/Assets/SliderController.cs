6f7b067b8be02d9774729f03f5521408c623e70e
Samuel Hegner <C16439262@mydit.ie>
2020-04-30 15:01:52 +0100

    Controls Functionality

---
+++ Journey 3.0/Assets/SliderController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderController : MonoBehaviour
+{
+    [SerializeField] private GameObject gamepadImage, keyboardImage, gamepadText, keyboardText;
+
+    public void ChangeSlide()
+    {
+        gamepadImage.SetActive(keyboardImage.activeSelf);
+        keyboardImage.SetActive(!gamepadImage.activeSelf);
+        gamepadText.SetActive(keyboardText.activeSelf);
+        keyboardText.SetActive(!gamepadText.activeSelf);
+    }
+}
