914e87f385a8f0c1ad842e2aba5acf585b50b24c
Samuel Hegner <C16439262@mydit.ie>
2020-05-01 12:50:21 +0100

    Analytics Adjusted

--- Journey 3.0/Assets/Scripts/AnalyticsSendFrameRate.cs
+++ Journey 3.0/Assets/Scripts/AnalyticsSendFrameRate.cs
@@ -7,7 +7,7 @@ public class AnalyticsSendFrameRate : MonoBehaviour
 {
     public void SendAvgFrameRate(){
         //Send the event. Also get the result, so we can make sure it sent correctly
-
+        
         if (!Application.isEditor)
         {
             AnalyticsResult result = AnalyticsEvent.Custom("average_frameRate", new Dictionary<string, object>
