ea00be3854a257aa3735860fb5555e3d459dab11
Samuel Hegner <C16439262@mydit.ie>
2020-05-06 17:10:01 +0100

    Gallery Nearly finished

---
+++ Journey 3.0/Assets/GalleryScroller.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GalleryScroller : MonoBehaviour
+{
+    [SerializeField] private int pageNumber = 0;
+    
+    [SerializeField] private UIPage[] _pages;
+
+    public void ScrollUp()
+    {
+        if (pageNumber == 1)
+        {
+            _pages[0].gameObject.SetActive(true);
+            _pages[1].gameObject.SetActive(false);
+            _pages[2].gameObject.SetActive(false);
+            
+            //SetDefaultEventSystem(_pages[0].defaultSelect);
+            pageNumber = 0;
+        }else if (pageNumber == 2)
+        {
+            _pages[0].gameObject.SetActive(false);
+            _pages[1].gameObject.SetActive(true);
+            _pages[2].gameObject.SetActive(false);
+            
+            //SetDefaultEventSystem(_pages[1].defaultSelect);
+            pageNumber = 1;
+        }
+    }
+    
+    public void ScrollDown()
+    {
+        if (pageNumber == 0)
+        {
+            _pages[0].gameObject.SetActive(false);
+            _pages[1].gameObject.SetActive(true);
+            _pages[2].gameObject.SetActive(false);
+            
+            //SetDefaultEventSystem(_pages[1].defaultSelect);
+            pageNumber = 1;
+        }else if(pageNumber == 1)
+        {
+            _pages[0].gameObject.SetActive(false);
+            _pages[1].gameObject.SetActive(false);
+            _pages[2].gameObject.SetActive(true);
+            
+            //SetDefaultEventSystem(_pages[2].defaultSelect);
+            pageNumber = 2;
+        }
+    }
+
+    void SetDefaultEventSystem(GameObject obj)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(obj);
+    }
+}
---
+++ Journey 3.0/Assets/UIPage.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPage : MonoBehaviour
+{ 
+    public GameObject defaultSelect;
+}
