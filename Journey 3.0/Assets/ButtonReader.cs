0ae04deeae2753ccfdea17412f8c6a43e89b05d1
Samuel Hegner <C16439262@mydit.ie>
2020-05-06 17:57:22 +0100

    Button Reader (Hell Yeah)

---
+++ Journey 3.0/Assets/ButtonReader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonReader : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler, IDeselectHandler
+{
+    [SerializeField] private Animator _animator;
+    
+    // When highlighted with mouse.
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //ChangeSelection to be the hover over button
+        EventSystem.current.SetSelectedGameObject(gameObject);
+        print("Play Animation on Pointer");
+    }
+    
+    //When un-highlighted with mouse
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+
+    // When selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        print("Play Animation on Select");
+    }
+
+    //When un-selected
+    public void OnDeselect(BaseEventData eventData)
+    {
+        //To do: turn off highlighted buttons
+        GetComponent<Selectable>().OnPointerExit(null);
+    }
+}
