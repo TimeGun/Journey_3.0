31f00b1d6f91fad72a18ff93d47a6af040fe4896
Samuel Hegner <C16439262@mydit.ie>
2020-05-07 17:54:50 +0100

    Smoking Flower

---
+++ Journey 3.0/Assets/FlowerSmokeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class FlowerSmokeController : MonoBehaviour
+{
+    public static FlowerSmokeController instance;
+
+    [SerializeField] private VisualEffect _smokingFlowerVFX;
+
+    private Coroutine _coroutine;
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void StartSmoking(float timeToSmokeFor)
+    {
+        StopAllCoroutines();
+        _coroutine = StartCoroutine(FlowerSmoking(timeToSmokeFor));
+    }
+
+    private IEnumerator FlowerSmoking(float value)
+    {
+        _smokingFlowerVFX.SetBool("isSpawning", true);
+        yield return new WaitForSeconds(value);
+        
+        _smokingFlowerVFX.SetBool("isSpawning", false);
+
+    }
+}
---
+++ Journey 3.0/Assets/SmokingStart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokingStart : MonoBehaviour
+{
+    public float smokeTime = 10f;
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FlowerSmokeController.instance.StartSmoking(smokeTime);
+        }
+    }
+}
