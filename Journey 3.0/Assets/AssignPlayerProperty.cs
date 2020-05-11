e853246eaeb856b1d3eb6dbb4e3fdb5fdd3b1d27
Samuel Hegner <C16439262@mydit.ie>
2020-05-11 18:33:14 +0100

    fIREFLY  sphere

---
+++ Journey 3.0/Assets/AssignPlayerProperty.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using UnityEngine;
+
+
+public class AssignPlayerProperty : MonoBehaviour
+{
+    private Transform _player;
+    private void Start()
+    {
+        _player = API.GlobalReferences.PlayerRef.transform;
+    }
+
+    private void Update()
+    {
+        transform.position = _player.position + new Vector3(0, 0.5f, 0);
+    }
+}
