eb31771604ce38586d9dcabffec7131dfff9335d
Samuel Hegner <C16439262@mydit.ie>
2020-05-11 16:34:58 +0100

    Audio Adjustments Elevator

---
+++ Journey 3.0/Assets/FadeEngineNoise.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeEngineNoise : MonoBehaviour
+{
+    public void FadeNoise()
+    {
+        FadeOutVolume[] engines = FindObjectsOfType<FadeOutVolume>();
+
+        foreach (var engine in engines)
+        {
+            engine.FadeOut();
+        }
+    }
+}
---
+++ Journey 3.0/Assets/FadeOutVolume.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutVolume : MonoBehaviour
+{
+    [SerializeField] private AudioSource _engineSource;
+    [SerializeField] private float fadeSpeed = 0.75f;
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutEngine());
+    }
+
+    IEnumerator FadeOutEngine()
+    {
+        while (_engineSource.volume > 0)
+        {
+            _engineSource.volume = Mathf.Lerp(_engineSource.volume, 0, Time.deltaTime * fadeSpeed);
+            yield return null;
+        }
+    }
+}
