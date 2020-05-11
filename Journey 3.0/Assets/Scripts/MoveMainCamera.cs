904dfcf631d5bd052bc6aa47a3ea059a612fe2ec
gazor117 <oreillygary7@gmail.com>
2020-05-11 20:13:25 +0100

    Start of End Cinematic

---
+++ Journey 3.0/Assets/Scripts/MoveMainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MoveMainCamera : MonoBehaviour
+{
+
+    private GameObject target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Main Camera");
+        SceneManager.MoveGameObjectToScene(target,SceneManager.GetSceneByName("Section10") );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveCamera()
+    {
+        
+        
+    }
+}
