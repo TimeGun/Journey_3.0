3570ac9393dd51cfdc7c0a442597247d798d76f9
Samuel Hegner <C16439262@mydit.ie>
2020-05-13 12:55:00 +0100

    Torch drop Section 2

---
+++ Journey 3.0/Assets/DropTorchS2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTorchS2 : MonoBehaviour
+{
+    public void DropObject()
+    {
+        API.GlobalReferences.PlayerRef.GetComponent<InteractWithObject>().StopInteracting();
+    }
+}
