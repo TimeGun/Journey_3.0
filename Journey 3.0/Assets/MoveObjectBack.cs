b1170a9f2315b599ced3f1a3ca560047edf5f014
Samuel Hegner <C16439262@mydit.ie>
2020-05-14 11:25:53 +0100

    Section 3 Failsafe

---
+++ Journey 3.0/Assets/MoveObjectBack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectBack : MonoBehaviour
+{
+    [SerializeField] private Transform resetLocation;
+    private void OnTriggerEnter(Collider other)
+    {
+        IInteractible interactible = other.GetComponent<IInteractible>();
+
+        if (interactible != null)
+        {
+            interactible.getGameObject().transform.position = resetLocation.position;
+        }
+    }
+}
