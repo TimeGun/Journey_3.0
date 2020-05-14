0e9a4b5112eba192b45698921f04e1da342673fe
Samuel Hegner <C16439262@mydit.ie>
2020-05-14 22:06:49 +0100

    Gear Ticking

---
+++ Journey 3.0/Assets/TickingController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TickingController : MonoBehaviour
+{
+
+    public AudioSource _source;
+
+    [SerializeField] private AudioClip _normal;
+    [SerializeField] private AudioClip _impact;
+
+
+    private int tickCounter;
+
+
+    public void TickSound()
+    {
+        if (tickCounter % 3 == 0)
+        {
+            _source.PlayOneShot(_impact);
+        }
+        else
+        {
+            _source.PlayOneShot(_normal);
+        }
+        
+        tickCounter++;
+    }
+}
