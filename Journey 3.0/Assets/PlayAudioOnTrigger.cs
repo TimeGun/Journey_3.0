14f5a05404c9469dbaf65e5a013b43bc40fc61b9
Samuel Hegner <C16439262@mydit.ie>
2020-05-14 11:27:57 +0100

    gate close plays once

--- Journey 3.0/Assets/PlayAudioOnTrigger.cs
+++ Journey 3.0/Assets/PlayAudioOnTrigger.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class PlayAudioOnTrigger : MonoBehaviour
 {
     [SerializeField]private AudioSource _source;
+
+    private bool played = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && played == false)
         {
             print("Playing Audio");
             _source.Play();
+            played = true;
         }
     }
 }
