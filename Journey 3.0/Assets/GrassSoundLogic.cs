1e43a44b26f2ee7524830187f58ea5d7cb645a6e
Samuel Hegner <C16439262@mydit.ie>
2020-06-03 16:37:05 +0100

    Grass Sound in (Terrible sfx)

---
+++ Journey 3.0/Assets/GrassSoundLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassSoundLogic : MonoBehaviour
+{
+    public Transform sourceObject;
+
+    private Transform player;
+
+    public AudioSource audioSource;
+
+    private CharacterController cc;
+
+    private bool within = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger == false)
+        {
+            player = other.transform;
+            cc = player.GetComponent<CharacterController>();
+            within = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger == false)
+        {
+            within = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (within)
+        {
+            if (cc.velocity.magnitude > 0.5f)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.Play();
+                }
+                sourceObject.position = player.position;
+            }
+            else
+            {
+                if (audioSource.isPlaying)
+                {
+                    audioSource.Pause();
+                }
+            }
+        }
+    }
+}
