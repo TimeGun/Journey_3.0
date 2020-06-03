e16028406d3610e67c731552b713157b2955ed13
Samuel Hegner <C16439262@mydit.ie>
2020-06-03 20:38:07 +0100

    Demo Changes

--- Journey 3.0/Assets/DropTorchS2.cs
+++ Journey 3.0/Assets/DropTorchS2.cs
@@ -6,6 +6,11 @@ public class DropTorchS2 : MonoBehaviour
 {
     public void DropObject()
     {
+        Invoke("DropTorch", 0.5f);
+    }
+
+    void DropTorch()
+    {
         API.GlobalReferences.PlayerRef.GetComponent<InteractWithObject>().StopInteracting();
     }
 }
