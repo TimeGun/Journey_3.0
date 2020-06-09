d384e79d4c857ac4e75d5ec17ba298429187f116
Samuel Hegner <C16439262@mydit.ie>
2020-06-09 11:42:19 +0100

    Restart Timeline

---
+++ Journey 3.0/Assets/RestartTimeline.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class RestartTimeline : MonoBehaviour
+{
+    private PlayableDirector _timeline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timeline = GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _timeline.Stop();
+            _timeline.Play();
+        }
+    }
+}
