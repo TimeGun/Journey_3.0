810df9a4d3fb76c669002dc4298c34e667e6e3e1
Samuel Hegner <C16439262@mydit.ie>
2020-06-23 15:14:28 +0100

    Update PlayTimeline.cs

--- Journey 3.0/Assets/Scripts/PlayTimeline.cs
+++ Journey 3.0/Assets/Scripts/PlayTimeline.cs
@@ -7,61 +7,62 @@ using UnityEngine.Playables;
 public class PlayTimeline : MonoBehaviour
 {
     public GameObject cameraTrigger;
-    private bool _playerEntered;
+    public GameObject targetCamera;
+
+    private DetectPlayer _detectPlayer;
+    
     public PlayableDirector playableDirector;
+    
+    private CinemachineVirtualCamera vcam;
+
     private int _startPriority;
     public int targetPriority;
-    public GameObject targetCamera;
-    private CinemachineVirtualCamera vcam;
+    
     private double _timelineLength;
+    
     private bool cinematicStarted;
-    public float playerFreezeTime;
     public bool freezePlayerForEntireCinematic;
     public bool blackBarsEnabled;
-
+    
+    public float playerFreezeTime;
     public float timeBeforeFreeze;
-    // Start is called before the first frame update
+
     void Start()
     {
         _timelineLength = playableDirector.duration;
         vcam = targetCamera.GetComponent<CinemachineVirtualCamera>();
         _startPriority = vcam.Priority;
+
+        if (cameraTrigger != null)
+        {
+            _detectPlayer = GetComponent<DetectPlayer>();
+        }
+
+        
         if (freezePlayerForEntireCinematic)
         {
             playerFreezeTime = (float) _timelineLength;
         }
     }
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-        if (cameraTrigger != null)
-        {
-            _playerEntered = cameraTrigger.GetComponent<DetectPlayer>().PlayerEntered;    //Use a coroutine to check if the player has entered every x amount of frames
-
-        }
-//        Debug.Log(_playerEntered);
-        if (_playerEntered && cinematicStarted == false)
+        if (_detectPlayer.PlayerEntered && cinematicStarted == false)
         {
             StartCoroutine(StartTimeline());
-//            Debug.Log("Here");
             cinematicStarted = true;
-
-
         }
     }
 
 
     IEnumerator StartTimeline()
     {
-        
-        
-       
         LevelSelectEnabler.DisableButton();
         vcam.Priority = targetPriority;
         playableDirector.Play();
         CinematicPlayerMoment.instance.FreezePlayer(timeBeforeFreeze, playerFreezeTime, blackBarsEnabled);
-        yield return new WaitForSeconds((float)_timelineLength);
+        yield return new WaitForSeconds((float) _timelineLength);
         vcam.Priority = _startPriority;
         LevelSelectEnabler.EnableButton();
     }
