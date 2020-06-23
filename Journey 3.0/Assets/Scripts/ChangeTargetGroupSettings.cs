638901113d0319f06e3556777386d27bce85adbd
Samuel Hegner <C16439262@mydit.ie>
2020-06-23 15:44:21 +0100

    Target Group Cleanup

--- Journey 3.0/Assets/Scripts/ChangeTargetGroupSettings.cs
+++ Journey 3.0/Assets/Scripts/ChangeTargetGroupSettings.cs
@@ -5,80 +5,57 @@ using Cinemachine;
 
 public class ChangeTargetGroupSettings : MonoBehaviour
 {
-    
     public CinemachineTargetGroup TG;
-    public bool afterTrigger;
-   [Tooltip("Index value of target")] public int targetIndex;
+    
+    [Tooltip("Index value of target")] public int targetIndex;
     public float weight1;
-    public float weight2;    
+    public float weight2;
+    
     private float weightValue;
+    
     public GameObject cameraTrigger1;
     public GameObject cameraTrigger2;
-    private bool firstTrigger;
-    private bool secondTrigger;
+
+
+    private DetectPlayer _trigger1;
+    private DetectPlayer _trigger2;
+    
 
     public float rateOfChange;
-    
-    
-    
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
-        
-
+        _trigger1 = cameraTrigger1.GetComponent<DetectPlayer>();
+        _trigger2 = cameraTrigger2.GetComponent<DetectPlayer>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        firstTrigger = cameraTrigger1.GetComponent<DetectPlayer>().PlayerEntered;   //Use this to trigger when  offset decreases
-        secondTrigger = cameraTrigger2.GetComponent<DetectPlayer>().PlayerEntered;  //Use this to trigger when  offset increases
-
-        
-        if (firstTrigger)
+        if (_trigger1.PlayerEntered)
         {
-            afterTrigger = false;        //this is used to begin decreasing the value of the offset
             StopAllCoroutines();
             StartCoroutine(MoveToValue(weight1));
-            
         }
-        else if (secondTrigger)
+        else if (_trigger2.PlayerEntered)
         {
-            afterTrigger = true;    //this is used to begin increasing the value of the offset
             StopAllCoroutines();
             StartCoroutine(MoveToValue(weight2));
         }
-        
-        if (afterTrigger)
-                {
-                    //weightValue = weight2;
-                    
-                    
-                }
-                else
-                {
-                    //weightValue = weight1;
-                    
-                }
-        
-        //TG.m_Targets[targetIndex].weight = weightValue;
     }
 
     IEnumerator MoveToValue(float newWeight)
     {
-        Debug.Log("In Coroutine");
-
-        while ( TG.m_Targets[targetIndex].weight > newWeight + 0.01 || TG.m_Targets[targetIndex].weight < newWeight - 0.01)
+        while (TG.m_Targets[targetIndex].weight > newWeight + 0.01 ||
+               TG.m_Targets[targetIndex].weight < newWeight - 0.01)
         {
-            Debug.Log(newWeight);
-            Debug.Log(TG.m_Targets[targetIndex].weight);
-            float tempWeight = Mathf.Lerp(TG.m_Targets[targetIndex].weight, newWeight, rateOfChange/100 * Time.deltaTime);
-            Debug.Log(tempWeight);
+            float tempWeight = Mathf.Lerp(TG.m_Targets[targetIndex].weight, newWeight,
+                rateOfChange / (100f * Time.deltaTime));
+            
             TG.m_Targets[targetIndex].weight = tempWeight;
-            yield return new WaitForSeconds(Time.deltaTime);
-
+            
+            
+            yield return new WaitForEndOfFrame();
         }
     }
-    
 }
