b6a39ad5389e7484891a05e61fc88c4d6fbfa2bd
Samuel Hegner <C16439262@mydit.ie>
2020-07-01 11:02:56 +0100

    Nothing is free 2.0

--- Journey 3.0/Assets/Scripts/FireflyRef.cs
+++ Journey 3.0/Assets/Scripts/FireflyRef.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class FireflyRef : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         PlayerManager.FireflyRef = transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Journey 3.0/Assets/Scripts/FireflyTrigger.cs
+++ Journey 3.0/Assets/Scripts/FireflyTrigger.cs
@@ -5,12 +5,8 @@ using UnityEngine;
 using Cinemachine;
 
 
-
-
-
 public class FireflyTrigger : MonoBehaviour
 {
-    
     [SerializeField] private float _fleeSpeed;
     public GameObject Camera;
     [SerializeField] private Transform _flies;
@@ -19,32 +15,27 @@ public class FireflyTrigger : MonoBehaviour
     private ParticleSystem.MainModule _mm;
     private ParticleSystem.VelocityOverLifetimeModule _vol;
 
-    
-    
+
     private bool _flee;
 
     [SerializeField] private float _timeToFleeFor;
-    
+
     private float _timeSinceFlee;
 
     [SerializeField] public int twinkleAmmountToAdd = 10;
-
-    private void Start()
-    {
-        //_system.Stop();        //Gary Added
-        
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
-        _system.Play();    //Gary Added 
+        _system.Play(); //Gary Added 
         StartCoroutine(Flee());
     }
 
     IEnumerator Flee()
     {
         FlyTwinkleController.instance.AddTwinkleAmount(twinkleAmmountToAdd);
+        
         _flee = true;
+        
         while (_flee)
         {
             _flies.transform.Translate(Vector3.up * (_fleeSpeed * Time.deltaTime));
@@ -53,15 +44,16 @@ public class FireflyTrigger : MonoBehaviour
             {
                 _flee = false;
             }
+
             yield return new WaitForEndOfFrame();
         }
 
         _em = _system.emission;
         _mm = _system.main;
-        
+
         _em.rateOverDistance = 0;
         _em.rateOverTime = 0;
-        
+
         /*_vol = _system.velocityOverLifetime;
         _vol.x = new ParticleSystem.MinMaxCurve(-5,5);
         _vol.y = new ParticleSystem.MinMaxCurve(8,12);
@@ -69,8 +61,8 @@ public class FireflyTrigger : MonoBehaviour
 
         yield return new WaitForSeconds(_mm.startLifetime.Evaluate(1));
         _system.Stop();
-        
-        
+
+
         Destroy(gameObject);
     }
 }
