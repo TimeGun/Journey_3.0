8809856580293f818ca76adb27d60a929702f2b7
Samuel Hegner <C16439262@mydit.ie>
2020-07-01 10:52:21 +0100

    Update EnableObject.cs

--- Journey 3.0/Assets/Scripts/EnableObject.cs
+++ Journey 3.0/Assets/Scripts/EnableObject.cs
@@ -7,23 +7,23 @@ public class EnableObject : MonoBehaviour
     public GameObject Target;
     public GameObject cameraTrigger;
     private bool playerInZone;
+
+    private DetectPlayer detectPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        //Target = GameObject.Find("CameraTrigger6");
+        detectPlayer = cameraTrigger.GetComponent<DetectPlayer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerInZone = cameraTrigger.GetComponent<DetectPlayer>().PlayerInCollider;
-        if (playerInZone && Target != null && Target.activeSelf == false)
+        if (detectPlayer.PlayerInCollider && Target != null && Target.activeSelf == false)
         {
             Target.gameObject.SetActive(true);
             //Target.GetComponent<DetectPlayer>().enabled = false;
             //Debug.Log("player oi");
         }
-        
-       
     }
 }
