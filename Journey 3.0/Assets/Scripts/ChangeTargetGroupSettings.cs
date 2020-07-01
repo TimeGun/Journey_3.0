e4826390beb6305013e9648597103901fe8156da
Samuel Hegner <C16439262@mydit.ie>
2020-07-01 10:35:26 +0100

    Gradual Loading Target Group Settings

--- Journey 3.0/Assets/Scripts/ChangeTargetGroupSettings.cs
+++ Journey 3.0/Assets/Scripts/ChangeTargetGroupSettings.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class ChangeTargetGroupSettings : MonoBehaviour
+public class ChangeTargetGroupSettings : GradualLoader
 {
     public CinemachineTargetGroup TG;
-    
+
     [Tooltip("Index value of target")] public int targetIndex;
     public float weight1;
     public float weight2;
-    
+
     private float weightValue;
-    
+
     public GameObject cameraTrigger1;
     public GameObject cameraTrigger2;
 
 
     private DetectPlayer _trigger1;
     private DetectPlayer _trigger2;
-    
+
 
     public float rateOfChange;
 
     private float val;
 
 
-    void Start()
+    IEnumerator Start()
     {
+        yield return new WaitUntil(() => initialised);
         _trigger1 = cameraTrigger1.GetComponent<DetectPlayer>();
         _trigger2 = cameraTrigger2.GetComponent<DetectPlayer>();
         val = weight1;
@@ -36,17 +37,20 @@ public class ChangeTargetGroupSettings : MonoBehaviour
 
     void Update()
     {
-        if (_trigger1.PlayerEntered)
+        if (updateReady)
         {
-            //StopAllCoroutines();
-            print("Started MoveVAl");
-            val = weight1;
-        }
-        else if (_trigger2.PlayerEntered)
-        {
-            //StopAllCoroutines();
-            print("Started MoveVAl");
-            val = weight2;
+            if (_trigger1.PlayerEntered)
+            {
+                //StopAllCoroutines();
+                print("Started MoveVAl");
+                val = weight1;
+            }
+            else if (_trigger2.PlayerEntered)
+            {
+                //StopAllCoroutines();
+                print("Started MoveVAl");
+                val = weight2;
+            }
         }
     }
 
@@ -58,24 +62,24 @@ public class ChangeTargetGroupSettings : MonoBehaviour
                 TG.m_Targets[targetIndex].weight < val - 0.01)
             {
                 float tempWeight = Mathf.Lerp(TG.m_Targets[targetIndex].weight, val,
-                    (1f/rateOfChange) * Time.deltaTime);
-             
-                TG.m_Targets[targetIndex].weight = tempWeight;
+                    (1f / rateOfChange) * Time.deltaTime);
 
+                TG.m_Targets[targetIndex].weight = tempWeight;
             }
+
             yield return null;
         }
 
-    /*
-        while (TG.m_Targets[targetIndex].weight > newWeight + 0.01 ||
-               TG.m_Targets[targetIndex].weight < newWeight - 0.01)
-        {
-            float tempWeight = Mathf.Lerp(TG.m_Targets[targetIndex].weight, newWeight,
-                (1f/rateOfChange) * Time.deltaTime);
-             
-            TG.m_Targets[targetIndex].weight = tempWeight;
-
-            yield return null;
-        }*/
+        /*
+            while (TG.m_Targets[targetIndex].weight > newWeight + 0.01 ||
+                   TG.m_Targets[targetIndex].weight < newWeight - 0.01)
+            {
+                float tempWeight = Mathf.Lerp(TG.m_Targets[targetIndex].weight, newWeight,
+                    (1f/rateOfChange) * Time.deltaTime);
+                 
+                TG.m_Targets[targetIndex].weight = tempWeight;
+    
+                yield return null;
+            }*/
     }
 }
