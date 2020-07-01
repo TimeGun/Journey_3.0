0366a06121584e5651454173c46cf8d92c02f888
Samuel Hegner <C16439262@mydit.ie>
2020-07-01 10:23:35 +0100

    Update AnimatedPositionDebug.cs

--- Journey 3.0/Assets/Scripts/AnimatedPositionDebug.cs
+++ Journey 3.0/Assets/Scripts/AnimatedPositionDebug.cs
@@ -9,18 +9,6 @@ public class AnimatedPositionDebug : MonoBehaviour
 
     [SerializeField] private float positionVerticalAdjustment;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
