cd04ab9674ba303354da089057c3766532c11fca
Samuel Hegner <C16439262@mydit.ie>
2020-07-08 16:06:43 +0100

    Achievement S3 puzzle

---
+++ Journey 3.0/Assets/UnlockAchievement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockAchievement : MonoBehaviour
+{
+    public AchievementSO achievementToUnlock;
+
+    public void UnlockAchievementSO()
+    {
+        if (achievementToUnlock != null)
+            AchievementManager.instance.UnlockSteamAchievement(achievementToUnlock);
+    }
+}
