57383a753ad7653e5e67210463f961a89407656e
Samuel Hegner <C16439262@mydit.ie>
2020-07-15 16:28:18 +0100

    Controls selection Fix

---
+++ Journey 3.0/Assets/SetControlsSelected.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetControlsSelected : MonoBehaviour
+{
+    public enum ControlsToShow
+    {
+        controller,
+        keyboard
+    }
+
+    [SerializeField] private GameObject right;
+    [SerializeField] private GameObject left;
+
+    public ControlsToShow select;
+
+    public void SetCorrectSelected()
+    {
+        if (select == ControlsToShow.controller)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(left);
+        }
+        else if(select == ControlsToShow.keyboard)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(right);
+        }
+    }
+
+    
+}
