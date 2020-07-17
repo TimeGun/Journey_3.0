10f45a158c074e51dee967ca83627ac30453dca4
Samuel Hegner <C16439262@mydit.ie>
2020-07-17 11:44:04 +0100

    Menu Fully fixed

--- Journey 3.0/Assets/Scripts/MenuController.cs
+++ Journey 3.0/Assets/Scripts/MenuController.cs
@@ -350,7 +350,7 @@ public class MenuController : MonoBehaviour
         if (fadeAlpha)
             baseGroup.alpha = alphaValue;
         
-        yield return new WaitForSecondsRealtime(0.08f);
+        yield return new WaitForSecondsRealtime(0.15f);
         cooldown = false;
     }
 
@@ -370,7 +370,7 @@ public class MenuController : MonoBehaviour
         if (fadeAlpha)
             baseGroup.alpha = 1f;
         
-        yield return new WaitForSecondsRealtime(0.08f);
+        yield return new WaitForSecondsRealtime(0.15f);
         cooldown = false;
     }
 
