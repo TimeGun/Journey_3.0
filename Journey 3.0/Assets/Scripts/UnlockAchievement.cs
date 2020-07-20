b60caae0af4dd1ca408f872354ef260d2649ab4e
Samuel Hegner <C16439262@mydit.ie>
2020-07-20 13:41:05 +0100

    fixed stargazer

--- Journey 3.0/Assets/Scripts/UnlockAchievement.cs
+++ Journey 3.0/Assets/Scripts/UnlockAchievement.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class UnlockAchievement : MonoBehaviour
 {
     public AchievementSO achievementToUnlock;
 
+    [SerializeField] private VisualEffect shower;
+
+    private bool triggered;
+
     public void UnlockAchievementSO()
     {
-        if (achievementToUnlock != null)
-            AchievementManager.instance.UnlockSteamAchievement(achievementToUnlock);
+        if (!triggered)
+        {
+            if (achievementToUnlock != null)
+                AchievementManager.instance.UnlockSteamAchievement(achievementToUnlock);
+            
+            shower.SendEvent("MeteorShowerSpawn");
+            triggered = true;
+        }
     }
 }
